4fa0d920a824f12151ab2813cd54465fce8b7130
kalnynsh <kda869@yandex.ru>
2018-03-01 16:40:02 +0300

    Added to form datepicker for Birthday.

--- js/script02.js
+++ js/script02.js
@@ -14,5 +14,23 @@
     }).fail(function() {
       console.log("Fail to load JSON`s Towns List");
     });
+
+    $("#date-pickerID").datepicker({
+      monthNames: [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь"
+      ],
+      dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+      firstDay: 1,
+      dateFormat: "dd.mm.yy"
+    });
   });
 })(jQuery);
